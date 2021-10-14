7c84a8e6619fa71ff9f46ab74e071ea61ad6e53a
Ashokbobby <beeravalliashokreddy@gmail.com>
2021-10-14 12:50:36 +0530

    #AC-3521 Changed to workable format

--- src/main/java/learningjunit/Caluclator.java
+++ src/main/java/learningjunit/Caluclator.java
@@ -1,9 +1,14 @@
 package learningjunit;
 
 public class Caluclator {
+	public static void main(String[] args) {
+		int  added =add(3,4);
+		System.out.println(added);
+
+	}
 	public static int add(int a,int b) {
 		return a+b;
-		//add tyhe new comment
+		//added new comment
 	}
 
 }
